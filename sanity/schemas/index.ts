fafe2641606523b7ad790ee490eea93a92a01c6a
masternnind <masternnind@gmail.com>
2025-08-24 20:57:47 +0900

    Delete sanity directory

--- sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import post from "./post";
-import project from "./project";
-import tag from "./tag";
-import siteSettings from "./siteSettings";
-
-export const schemaTypes = [post, project, tag, siteSettings];
